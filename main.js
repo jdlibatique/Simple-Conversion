c7b6506bc424c7d3330a3c16e324f42c752122b7
Joseph Devereaux Libatique <me@jdlibatique.dev>
2022-06-23 11:50:29 +0800

    changed conversion method

--- main.js
+++ main.js
@@ -3,3 +3,17 @@ function convertToMilliliters() {
 		parseFloat(document.getElementById("milliliter").value) /
 		parseFloat(document.getElementById("imperial").value);
 }
+
+function convertOnKeyDown() {
+	document.getElementById("outputArea").value =
+		(parseFloat(document.getElementById("milliliter").value) /
+			parseFloat(document.getElementById("imperial").value)) *
+		parseFloat(document.getElementById("metric").value);
+}
+
+function convertOnKeyDownOutput() {
+	document.getElementById("milliliter").value =
+		(parseFloat(document.getElementById("imperial").value) *
+			parseFloat(document.getElementById("outputArea").value)) /
+		parseFloat(document.getElementById("metric").value);
+}
